113c4bf7817182eb3e9b3601dba684861f448614
kjoerss <k.joerss@gmail.com>
2020-04-01 23:50:40 +0200

    fgbcgvgfh

---
+++ nodeServer/bc.js
@@ -0,0 +1,34 @@
+
+const SHA256 = require(crypto-js/SHA256);
+class Block{
+    constructor(index, timestamp, message, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash;
+    }
+
+    calculateHash(){
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+    }
+}
+
+class BlockChain{
+    constructor(){
+        this.chain = [];
+    }
+
+    createGenisisBlock(){
+        return new Block(0, "01/01/1970", "Genisis block", "0");
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length -1];
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock
+    }
+}
