793ecd6556f06f12f2bacfe2dfde48212afd3993
kjoerss <k.joerss@gmail.com>
2020-04-01 23:08:00 +0200

    Simple Rest API

--- nodeServer/app.js
+++ nodeServer/app.js
@@ -4,3 +4,19 @@ var app = express();
 app.listen(3000, () => {
     console.log("Server running on port 3000");
 })
+
+app.get("/", (req, res) => {
+    res.json("Website");
+});
+
+app.get("/getBC", (req, res) => {
+   res.json("BC"); 
+});
+
+app.get("/getWork", (req, res) => {
+    res.json("Work");
+});
+
+app.post("/solution", (req, res) => {
+    console.log(req.body.solution);
+});
